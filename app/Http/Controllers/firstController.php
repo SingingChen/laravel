c2b5e050acc64da10aa1aa3d825b39bf80298396
SingingChen <docterwater@gmail.com>
2016-08-08 10:50:38 +0800

    處理首頁路由需求，傳回index view

--- app/Http/Controllers/firstController.php
+++ app/Http/Controllers/firstController.php
@@ -10,46 +10,7 @@ class firstController extends Controller
 {
     public function index()
     {
-        return "首頁";
-    }
-    public function contact_us()
-{
-    return"聯絡我們";
-}
-    public function login()
-    {
-        return"登入";
-    }
-    public function logout()
-    {
-        return"登出";
-    }
-    public function products()
-    {
-        return"商品";
-    }
-    public function products_category()
-    {
-        return"商品分類";
-    }
-    public function products_brand()
-    {
-        return"品牌";
-    }
-    public function products_detail($id)
-    {
-            return $id;
-    }
-    public function search($key_word)
-    {
-        return $key_word;
-    }
- public function cart()
-    {
-        return"購物車";
-    }
- public function checkout()
-    {
-        return"結帳";
+        return view("index");
+
     }
 }
